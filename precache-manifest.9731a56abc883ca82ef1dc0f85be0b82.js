3dee485f292215283d5f71ad6f3a46ef3028a231
markrajk <marko88rajkovic@gmail.com>
2019-06-02 13:42:44 +0200

    Updates

---
+++ precache-manifest.9731a56abc883ca82ef1dc0f85be0b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0b112b0887fb1fa3537c1893c3bf17",
+    "url": "/nba-teams/index.html"
+  },
+  {
+    "revision": "89c25345b4775d9cc96d",
+    "url": "/nba-teams/static/css/main.41c6d7d7.chunk.css"
+  },
+  {
+    "revision": "1df419addb4e81c6b8cd",
+    "url": "/nba-teams/static/js/2.991e9033.chunk.js"
+  },
+  {
+    "revision": "89c25345b4775d9cc96d",
+    "url": "/nba-teams/static/js/main.f99fdc64.chunk.js"
+  },
+  {
+    "revision": "693353c61c017b5c6518",
+    "url": "/nba-teams/static/js/runtime~main.c2476181.js"
+  },
+  {
+    "revision": "e69a6812dc00537ac204b0e47e507f01",
+    "url": "/nba-teams/static/media/logo.e69a6812.png"
+  }
+]);
